270e4796e55e566e5a44721d3bd16d5b4abdb959
Dibble <DeflatedPickle@gmail.com>
2018-06-02 06:44:11 +0100

    Changed to use the pkinter GridCanvas

--- src/window.py
+++ src/window.py
@@ -4,6 +4,8 @@
 
 import tkinter as tk
 
+import pkinter as pk
+
 
 class Window(tk.Tk):
     def __init__(self):
@@ -12,8 +14,8 @@ class Window(tk.Tk):
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
 
-        self.canvas_width = 500
-        self.canvas_height = 500
+        self.canvas_width = 501
+        self.canvas_height = 501
 
         self.mouse_x = 0
         self.mouse_y = 0
@@ -23,15 +25,15 @@ class Window(tk.Tk):
         self.create_widgets()
         self.place_widgets()
 
-        self.interval()
+        # self.interval()
 
-        self.bind("<Motion>", self.mouse_set)
-        self.bind("<B1-Motion>", self.mouse_set, "+")
+        self.bind("<Button-1>", self.draw, "+")
         self.bind("<B1-Motion>", self.draw, "+")
 
     def create_widgets(self):
-        self.canvas = tk.Canvas(width=self.canvas_width, height=self.canvas_height,
-                                background="white")
+        self.canvas = pk.GridCanvas(self, rows=self.canvas_height // 10, columns=self.canvas_width // 10,
+                                    width=self.canvas_width, height=self.canvas_height,
+                                    background="white")
 
     def place_widgets(self):
         self.canvas.grid(row=0, column=0)
@@ -49,4 +51,4 @@ class Window(tk.Tk):
         self.mouse_y = event.y
 
     def draw(self, event=None):
-        self.canvas.create_text(self.mouse_x, self.mouse_y, text=self.character, tag="drawn")
+        self.canvas.place_cell_location(self.canvas.create_text(0, 0, text=self.character, tag="drawn"), event.x, event.y)
