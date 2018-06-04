79f0335773a92eaacb276f3661a4fcc7f2604ebc
Dibble <DeflatedPickle@gmail.com>
2018-06-04 08:29:46 +0100

    Added an option to export as PostScript

--- src/menu.py
+++ src/menu.py
@@ -4,6 +4,7 @@
 
 import sys
 import tkinter as tk
+from tkinter import filedialog
 
 import menumaker as mm
 
@@ -18,6 +19,8 @@ class Menu(tk.Menu):
         mm.constructor(self, [
             ("file", {"items": ["new ~ctrl+n",
                                 "---",
+                                "export postscript",
+                                "---",
                                 "exit ~ctrl+q"]}),
             ("view", {"items": ["[grid_var]grid ~ctrl+g"]})
         ], scope=locals()["self"])
@@ -26,6 +29,16 @@ class Menu(tk.Menu):
 
     def new(self, *args):
         self.parent.canvas.clear_grid()
+
+    def export_postscript(self, *args):
+        file = filedialog.SaveAs(filetypes=[("PostScript", "*.ps")])
+        file.show()
+
+        if file is None:
+            return
+
+        self.parent.canvas.update()
+        self.parent.canvas.postscript(file=file.filename + ".ps", colormode="color")
 
     def exit(self, *args):
         sys.exit()
