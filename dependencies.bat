8a46c23fdd406adff141ecae489423ef30dd4146
Dibbo <DeflatedPickle@gmail.com>
2018-06-07 09:39:51 +0100

    Added a script to install dependencies

---
+++ dependencies.bat
@@ -0,0 +1,6 @@
+pip3 install pkinter --upgrade && ^
+pip3 install menumaker --upgrade && ^
+pip3 install pillow --upgrade && ^
+pip3 install pygame --upgrade && ^
+pip3 install pyopengl --upgrade && ^
+pip3 install pyopengltk --upgrade
